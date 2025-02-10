cbcafcf9b7d2693fd7fca6a632fc8df1f8b5c1f1
Enyel04 <angeles7595@gmail.com>
2025-02-10 08:40:24 -0400

    config for linux

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        $this->call(VoyagerDatabaseSeeder::class);
         $this->call(DataTypesTableSeeder::class);
         $this->call(DataRowsTableSeeder::class);
         $this->call(MenusTableSeeder::class);
