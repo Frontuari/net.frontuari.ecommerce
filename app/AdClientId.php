3111ba6fd645cc62f2a03bee7100ceb6e3f861bf
EvyRose <hectopiaes@gmail.com>
2025-04-02 15:58:16 -0400

    php adclient
    made a namespace for adclientid

---
+++ app/AdClientId.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AdClientId extends Model {
+    protected $table = 'ad_client_id'; // Change this if your table is singular
+};
